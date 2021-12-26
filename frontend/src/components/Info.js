028b4edd5b5dc7b1fff98b5566d44c124ba73fe8
TanjuMutlu <tanjumutlu0@gmail.com>
2021-12-26 16:19:59 +0300

    initial commit

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Grid, Button, Typography, IconButton } from "@material-ui/core";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import { Link } from "react-router-dom";
+
+const pages = {
+  JOIN: "pages.join",
+  CREATE: "pages.create",
+};
+
+export default function Info(props) {
+  const [page, setPage] = useState(pages.JOIN);
+
+  function joinInfo() {
+    return "Bir Odaya Katilip Oda sahibinin belirledigi sekilde diger insanlarla muzigi kontrol edebilirsin.";
+  }
+
+  function createInfo() {
+    return "Veya spotfy hesabini bagla ve bir oda olusturup partiyi baslat!";
+  }
+
+  useEffect(() => {
+    console.log("ran");
+    return () => console.log("cleanup");
+  });
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h4" variant="h4">
+          Muzik Odasi Nedir?
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="body1">
+          {page === pages.JOIN ? joinInfo() : createInfo()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <IconButton
+          onClick={() => {
+            page === pages.CREATE ? setPage(pages.JOIN) : setPage(pages.CREATE);
+          }}
+        >
+          {page === pages.CREATE ? (
+            <NavigateBeforeIcon />
+          ) : (
+            <NavigateNextIcon />
+          )}
+        </IconButton>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button color="secondary" variant="contained" to="/" component={Link}>
+          Geri
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
